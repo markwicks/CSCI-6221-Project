b33db82201ea439be4db5eed8557af8a9b26af9e
Mark Wicks <markwicks@Marks-MacBook-Pro.local>
2021-11-13 21:48:15 -0500

    First version with full functionality

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ from shapely.geometry import Point
 from shapely.geometry.polygon import Polygon
 
 
-data_path = "/Users/markwicks/Desktop/advanced software paradigms/CSCI-6221-Project/data/"
+data_path = "/Users/markwicks/Desktop/Mark/Advanced Software Paradigms/CSCI-6221-Project/data/"
 
 ###############################################################################
 
-def getCrimeData():
+def getCrimeData(data_path):
     data = pd.read_csv(data_path + "crime_2020.csv")
     data = data[["LATITUDE", "LONGITUDE", "SHIFT", "START_DATE", "METHOD", "OFFENSE"]]
     return(data)
@@ -48,7 +48,6 @@ def countCrimesInArea(lat, long, data, radiusInMeters):
         
         if convertDegreesToMeters(dist) < radiusInMeters:
            counter = counter+1
-           print(dist)
            
     return counter
     
@@ -58,7 +57,7 @@ def countCrimesInArea(lat, long, data, radiusInMeters):
 # whether a point (latitude & longitude) is inside DC.
 # - The first value in each point returned is the longitude, and the second
 #   is the latitude.
-def getShapeFileForDC(returnDataFrame=False):
+def getShapeFileForDC(data_path, returnDataFrame=False):
     with fiona.open(data_path + "/cb_2018_us_state_500k/cb_2018_us_state_500k.shp") as c:
          for record in c:
              if record.get('properties').get('STATEFP') == '11':
@@ -84,7 +83,7 @@ def getShapeFileForDC(returnDataFrame=False):
 # - long is Longitude
 # - Shape is a list of the latitude and longitude of each point giving the 
 #   outline of an area (in our case, DC)
-def isPointInDC(lat, long, shape, output):
+def isPointInDC(lat, long, shape, output = False):
     
    point = Point(long, lat)
    polygon = Polygon(shape)
@@ -130,7 +129,7 @@ def getLatLongRange(pointList, coordType):
 # while checking whether each point is in DC. If the point is in DC, count 
 # the crimes in the surrounding area and save the result in a dataset.
 # When done, output the dataset.
-def getRankingData(pointList, crimeData, metersInterval):
+def getRankingData(pointList, crimeData, metersInterval, debug=True):
     
     longMin, longMax = getLatLongRange(pointList, "longitude")
     latMin,  latMax  = getLatLongRange(pointList, "latitude")
@@ -145,35 +144,49 @@ def getRankingData(pointList, crimeData, metersInterval):
     in_DC_counter = 0
     not_in_DC_counter = 0
     
-    latArray = np.arange(longMin, longMax, degreesInterval)
-    longArray = np.arange(latMin, latMax, degreesInterval)
+    longArray = np.arange(longMin, longMax, degreesInterval)
+    latArray  = np.arange(latMin, latMax, degreesInterval)
+    
+    dataframe = pd.DataFrame(
+      columns = ['latitude', 'longitude', 'number_of_crimes']
+    )
     
     ##############################
     # Work laterally (longitude) #
     ##############################
     for currentLong in longArray:
-           
+        
+       if debug==True:
+          print(" --> Current Long: " + str(currentLong))
+          print("     Max Long: " + str(longMax))
+          print("     In DC. Counter: " + str(in_DC_counter))
+          print("     Not in DC. Counter: " + str(not_in_DC_counter))
+          print("")
+          
        #################################
        # Working vertically (latitude) #
-       ##################################
+       #################################
        for currentLat in latArray:
            
-           if False:
-              print(" --> Current Lat: " + str(currentLat) + 
-                    "  Max Lat: " + str(latMax) + "\n")
-              print(" --> In DC. Counter: " + str(in_DC_counter))
-              print(" --> Not in DC. Counter: " + str(not_in_DC_counter))
-                
-           if isPointInDC(currentLong, currentLat, pointList, False) == True:
+           if isPointInDC(currentLat, currentLong, pointList, False) == True:
                
-              x = countCrimesInArea(
+              numCrimes = countCrimesInArea(
                 lat            = currentLat, 
                 long           = currentLong,
                 data           = crimeData,
                 radiusInMeters = metersInterval
               )
               
+              tmp_row = {
+                'latitude': currentLat,
+                'longitude': currentLong,
+                'number_of_crimes': numCrimes
+              }
+              
+              dataframe = dataframe.append(tmp_row, ignore_index= True)
+              
               in_DC_counter += 1
+              
            else:
               not_in_DC_counter += 1
           
@@ -181,32 +194,103 @@ def getRankingData(pointList, crimeData, metersInterval):
            
        currentLong += degreesInterval
        
-    return in_DC_counter, not_in_DC_counter
-
+    # Sort by number of crimes
+    (
+     dataframe.
+     sort_values(by = 'number_of_crimes', ascending = True, inplace = True)
+    )
+     
+    dataframe.reset_index(inplace=True)
+    
+    dataframe["percentile_rank"] = (dataframe.index / dataframe.shape[0])*100
+       
+    return dataframe
 
 ##############################################################################  
+def makeRankingData(data_path, out_path, metersInterval, debug=False):
+    
+   # Gives list of latitudes and longitudes for DC boundary
+   # of type 'polygon'.
+   DC_shape_file = getShapeFileForDC(data_path)
 
-# Gives list of latitudes and longitudes for DC boundary
-# of type 'polygon'.
-DC_shape_file = getShapeFileForDC()
+   crimeData = getCrimeData(data_path)
 
-crimeData = getCrimeData()
+   rankingData = getRankingData(
+       pointList      = DC_shape_file,
+       crimeData      = crimeData,
+       metersInterval = metersInterval,
+       debug          = debug
+   )
 
-offense_value_counts = crimeData["OFFENSE"].value_counts()
+   if out_path is not None: 
+      rankingData.to_csv(out_path + "rankingData.csv")
+      
+   return rankingData
 
-numInDC, numNotInDC = getRankingData(DC_shape_file, crimeData, 1000)
+############################################################################## 
+def getQuery(latitude, longitude, data_path, metersInterval, debug=False):
+    
+    crimeData = getCrimeData(data_path)
+    
+    numCrimes = countCrimesInArea(
+       lat            = latitude, 
+       long           = longitude,
+       data           = crimeData,
+       radiusInMeters = metersInterval
+    )
+    
+    if debug==True: print(" --> Num crimes: " + str(numCrimes))
+    
+    rankingData = pd.read_csv(data_path + "rankingData.csv")
+    
+    for row_num in range(rankingData.shape[0]):
+        if numCrimes <= rankingData.iloc[row_num].number_of_crimes: 
+           perc = rankingData.iloc[row_num].percentile_rank 
+           print(" --> Percentile: " + str(round(perc, 1)) + "%")
+           return perc
+    
+##############################################################################
+    
+if False:
+   rankingData = makeRankingData(
+     data_path      = data_path, 
+     out_path       = data_path,
+     metersInterval = 1000,
+     debug          = True
+   )
+   
+if False:
+   x = getQuery(
+     latitude       = 38.859959,
+     longitude      = -76.969035,
+     data_path      = data_path,
+     metersInterval = 1000,
+     debug          = False
+   )
+
 
 if False:
     
+   DC_shape_file = getShapeFileForDC(data_path)
+
+   crimeData = getCrimeData(data_path)
+   
    # lincoln memorial
-   isPointInDC(lat=38.889248, long=-77.050636, shape=DC_shape_file, output=True) 
+   isPointInDC(
+     lat    = 38.889248,
+     long   = -77.050636,
+     shape  = DC_shape_file,
+     output = True
+   ) 
     
-   countCrimesInArea(
+   numCrimes = countCrimesInArea(
       lat            = 38.9014829636, 
       long           = -76.9332371122,
       data           = crimeData,
       radiusInMeters = 1000
    )
+   
+   offense_value_counts = crimeData["OFFENSE"].value_counts()
 
 
 
